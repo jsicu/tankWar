b92aa7d6dc2437487d3b0da78572a0ac8bddf7a8
linzhongqi <741167479@qq.com>
2019-03-14 19:19:36 +0800

    .3/14 整容手术

--- src/com/young/tank/Data.java
+++ src/com/young/tank/Data.java
@@ -8,10 +8,10 @@ public class Data {
 	public static final int WINDOW_WIDTH = 600;
 	public static final int WINDOW_HEIGHT = 480;
 	public static final int WINDOW_FPS = 100;
-	public static final String WINDOW_TITLE = "《坦克大战》author：钟志鹏";
-	public static final Image WINDOW_ICOIMG = Toolkit.getDefaultToolkit().getImage(new Data().getClass().getResource("ico.png"));
-	public static final Image TANK_PLAN = Toolkit.getDefaultToolkit().getImage(new Data().getClass().getResource("tank_sprite.png"));//tank_sprite//space_sprite_full1
-	public static final Image Start_Img = Toolkit.getDefaultToolkit().getImage(new Data().getClass().getResource("tank_main.png"));
+	public static final String WINDOW_TITLE = "《坦克大战》 作者：林中奇";
+	public static final Image WINDOW_ICOIMG = Toolkit.getDefaultToolkit().getImage("img/logo.png");
+	public static final Image TANK_PLAN = Toolkit.getDefaultToolkit().getImage("img/tankIcon.png");//tank_sprite//space_sprite_full1
+	public static final Image Start_Img = Toolkit.getDefaultToolkit().getImage("img/tankCover.png");
 	
 	
 	public static CopyOnWriteArrayList<Material> matarry = new CopyOnWriteArrayList<Material>();
